6d3b0c5c0186b26d6e727b4cdaf131e4a879a672
mudassir <muhammad.mudassir@tenpearls.com>
2016-05-11 15:17:04 +0500

    DISPLAYING DATA

--- app/app.component.js
+++ app/app.component.js
@@ -9,6 +9,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var hero_1 = require('./hero');
 var AppComponent = (function () {
     function AppComponent() {
         //simple way
@@ -23,17 +24,24 @@ var AppComponent = (function () {
         // }
         //way used in tutorial
         this.title = 'Tour of Heroes';
-        this.heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
-        this.myHero = this.heroes[0];
+        //heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+        this.heroes = [
+            new hero_1.Hero(1, 'Windstorm'),
+            new hero_1.Hero(13, 'Bombasto'),
+            new hero_1.Hero(15, 'Magneta'),
+            new hero_1.Hero(20, 'Tornado')
+        ];
+        this.myHero = this.heroes[1];
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>{{title}}</h1>\n\t\t\t\t<h2>My favorite hero is {{myHero}} </h2>\n\t\t\t\t<p>Heroes</p>\n\t\t\t\t<ul>\n\t\t\t\t<li *ngFor=\"let hero of heroes\"> \n\t\t\t\t{{hero}}\n\t\t\t\t</li>\n\t\t\t\t</ul>"
+            template: "<h1>{{title}}</h1>\n\t\t\t\t<h2>My favorite hero is {{myHero.name}} </h2>\n\t\t\t\t<p>Heroes</p>\n\t\t\t\t<ul>\n\t\t\t\t<li *ngFor=\"let hero of heroes\"> \n\t\t\t\t{{hero.name}}\n\t\t\t\t</li>\n\t\t\t\t</ul>\n\t\t\t\t <p *ngIf=\"heroes.length > 3\">There are many heroes!</p>"
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+//console.log(new Hero(1, 'Windstorm')) 
 //# sourceMappingURL=app.component.js.map
